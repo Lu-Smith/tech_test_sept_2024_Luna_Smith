a6d7471052fef27a64dab5168aefbc18885f2fdb
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-09 13:58:00 +0100

    Created PaymentBenchmarkAssesment.vue

---
+++ src/components/PaymentBenchmarkAssesment.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <h2>Payment vs. Benchmark Assesment</h2>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>Provider Name</th>
+            <th>Total Payment (€)</th>
+            <th>Total Benchmark (€)</th>
+            <th>Difference (€)</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
