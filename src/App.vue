4525056f6e0201c91a14f8a1dfd20a20ee2a661c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-09 14:03:45 +0100

    Updated App.vue
    Added YearlyTrendChart.vue

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <h1 class="text-3xl font-bold underline">Technical Test</h1>
   <p>Check whether my data provider payments are over or under benchmark</p>
   <PaymentBenchmarkAssesment />
+  <YearlyTrendChart />
 </template>
 
 <script setup lang="ts">
   import { ref, onMounted } from 'vue';
   import { getProductBenchmarks, getExchangeRates } from './data/api.ts';
   import PaymentBenchmarkAssesment from './components/PaymentBenchmarkAssesment.vue';
+  import YearlyTrendChart from './components/YearlyTrendChart.vue';
 
   onMounted(async () => {
   try {
