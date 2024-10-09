43f5157bf0227dbd14468e921c0857b3015f1003
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-09 15:03:13 +0100

    Updated logic in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <h1 class="text-3xl font-bold underline">Technical Test</h1>
   <p>Check whether my data provider payments are over or under benchmark</p>
-  <PaymentBenchmarkAssesment />
+  <PaymentBenchmarkAssesment :transactions="transactions" />
   <YearlyTrendChart />
 </template>
 
@@ -11,18 +11,81 @@
   import PaymentBenchmarkAssesment from './components/PaymentBenchmarkAssesment.vue';
   import YearlyTrendChart from './components/YearlyTrendChart.vue';
 
-  onMounted(async () => {
-  try {
-    // Fetch data 
-    const productBenchmarks = await getProductBenchmarks();
-    console.log('Product Benchmarks:', productBenchmarks);
+  interface Currency {
+    id: number;
+    name: string;
+    symbol: string;
+  }
 
+  interface Benchmark {
+    benchmark: number;
+    currency: Currency;
+    payment: number;
+    provider_name: string;
+    product_name: string;
+    start_date: string;
+    end_date: string;
+  }
+
+  interface Transaction {
+  provider_name: string;
+  payment: number;
+  benchmark: number;
+  currency: Currency;
+  date: string;
+}
+
+const transactions = ref<Transaction[]>([]);
+
+onMounted(async () => {
+  try {
+    const productBenchmarks = await getProductBenchmarks();
     const exchangeRates = await getExchangeRates();
-    console.log('Exchange Rates:', exchangeRates);
+
+    console.error(productBenchmarks);
+
+    transactions.value = processProviderData(productBenchmarks.product_benchmarks, exchangeRates.exchange_rates);
   } catch (error) {
     console.error('Failed to fetch data:', error);
   }
 });
+
+function processProviderData(benchmarks: Benchmark[], rates: any): Transaction[] {
+  const transactionList: Transaction[] = [];
+
+  for (const benchmark of benchmarks) {
+    const { provider_name, payment, currency, benchmark: benchmarkValue, start_date } = benchmark;
+
+    let convertedPayment = payment;
+    let convertedBenchmark = benchmarkValue;
+    if (currency.id !== 3) { 
+         // ID 3 is EUR
+         const exchangeRateEntry = rates.find(rate => 
+        rate.from_currency_id === currency.id && rate.to_currency_id === 3
+      );
+
+      if (exchangeRateEntry) {
+        const exchangeRate = exchangeRateEntry.exchange_rate;
+        convertedPayment = payment * exchangeRate;
+        convertedBenchmark = benchmarkValue * exchangeRate;
+      } else {
+        console.warn(`No exchange rate found for currency ID ${currency.id} to EUR`); // Log if no exchange rate is found
+      }
+    } else {
+      convertedPayment = payment;
+    }
+
+    transactionList.push({
+      provider_name,
+      payment: convertedPayment,
+      benchmark: convertedBenchmark,
+      currency,
+      date: start_date,
+    });
+  }
+
+  return transactionList;
+}
 </script>
 
 <style scoped>
