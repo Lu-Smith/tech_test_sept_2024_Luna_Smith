7f2cdb6ed0224e29a72d519140b71fac96b0e700
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-09 13:26:20 +0100

    Fixed the bug in the header in api.ts

--- src/data/api.ts
+++ src/data/api.ts
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 const BASE_URL = 'https://substantive.pythonanywhere.com';
 
-const AUTH_KEY = '590e3e17b6a26a8fcda726e2a91520e476e2c894';
+const API_KEY = ' 590e3e17b6a26a8fcda726e2a91520e476e2c894';
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
   headers: {
-    'Authorization': `Bearer ${AUTH_KEY}`,
+    'auth-key': API_KEY,
   },
 });
 
