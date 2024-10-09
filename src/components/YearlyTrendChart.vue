dfdc13cc08dc871ebd6670e7cdcb8e79eb2d8169
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-09 18:25:28 +0100

    Updated YearlyTrendChart.vue

--- src/components/YearlyTrendChart.vue
+++ src/components/YearlyTrendChart.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>
-    <h2>Yearly Payment Trends</h2>
+  <div v-if="years.length > 1"> 
+    <h4>Yearly Payment Trend</h4>
+    <canvas ref="chartCanvas"></canvas>
   </div>
 </template>
+
+<script setup lang="ts">
+
+interface Transaction {
+  provider_name: string;
+  payment: number;
+  benchmark: number;
+  date: string;
+}
+
+const props = defineProps<{ transactions: Transaction[] }>();
+
+</script>
+
+<style scoped>
+canvas {
+  max-width: 60%;
+  max-height: 400px;
+  margin: 20px 0;
+}
+</style>
