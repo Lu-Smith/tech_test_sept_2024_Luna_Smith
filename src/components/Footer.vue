c92ba1346a9fcc4567526336669c4d27fca0b1f1
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-10-10 11:26:54 +0100

    Created Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="bg-gray-800 text-white p-4 mt-8">
+    <h4 class="text-center text-sm md:text-base">
+      Coded by 
+      <a href="https://www.lunasmithart.com/" target="_blank" class="text-customGreenLink hover:text-customGreenHover">Luna Smith</a>,
+      <a href="https://github.com/Lu-Smith/tech_test_sept_2024_Luna_Smith" target="_blank" class="text-customGreenLink hover:text-customGreenHover">GitHub</a>.
+    </h4>
+  </footer>
+</template>
